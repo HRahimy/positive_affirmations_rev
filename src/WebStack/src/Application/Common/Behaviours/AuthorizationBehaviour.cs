60b308afc36ad09fe981ffc60913529b8af3bccd
HRahimy <rahimy.hamza@gmail.com>
2022-12-22 17:47:52 +0300

    Update packages to working level and fix build errors

--- src/WebStack/src/Application/Common/Behaviours/AuthorizationBehaviour.cs
+++ src/WebStack/src/Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
+using MediatR;
 using WebStack.Application.Common.Exceptions;
 using WebStack.Application.Common.Interfaces;
 using WebStack.Application.Common.Security;
-using MediatR;
 
 namespace WebStack.Application.Common.Behaviours;
 
-public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
     private readonly ICurrentUserService _currentUserService;
     private readonly IIdentityService _identityService;
@@ -19,7 +20,7 @@ public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRe
         _identityService = identityService;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
 
--- src/WebStack/src/Application/Common/Behaviours/PerformanceBehaviour.cs
+++ src/WebStack/src/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -1,11 +1,12 @@
 ﻿using System.Diagnostics;
-using WebStack.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using WebStack.Application.Common.Interfaces;
 
 namespace WebStack.Application.Common.Behaviours;
 
-public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
     private readonly Stopwatch _timer;
     private readonly ILogger<TRequest> _logger;
@@ -24,7 +25,7 @@ public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequ
         _identityService = identityService;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _timer.Start();
 
