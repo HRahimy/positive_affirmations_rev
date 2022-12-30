d5cc1f73e58c68689468c97966df8afa82cecb65
HRahimy <rahimy.hamza@gmail.com>
2022-12-30 21:44:17 +0300

    [WebStack] Apply temporary workaround for broken ApiAuthorizationDbContext implementation in latest version
    Also added notes for references describing the problem and source for the solution

--- src/WebStack/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/WebStack/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,11 +1,7 @@
 ﻿using System.Reflection;
-using Duende.IdentityServer.EntityFramework.Entities;
-using Duende.IdentityServer.EntityFramework.Extensions;
-using Duende.IdentityServer.EntityFramework.Interfaces;
 using Duende.IdentityServer.EntityFramework.Options;
 using MediatR;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using WebStack.Application.Common.Interfaces;
@@ -19,58 +15,34 @@ namespace WebStack.Infrastructure.Persistence;
 // References to WIP solution for resolving entity mapping issues during database initialization:
 // https://github.com/dotnet/aspnetcore/issues/44990#issuecomment-1318124064
 // https://stackoverflow.com/questions/71827951/asp-net-core-web-api-how-to-resolve-detected-package-version-outside-of-depend
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IPersistedGrantDbContext, IApplicationDbContext
+// Final outcome:
+// Accepting setting version of package 'Duende.IdentityServer.EntityFramework' to 6.0.4 as a temporary solution for now.
+// TODO: Update Identity related packages once an update is released that implements the latest interface of ApiAuthorizationDbContext that includes the ServerSideSessions property
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
 {
     private readonly IMediator _mediator;
     private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
-    private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
 
     public ApplicationDbContext(
         DbContextOptions<ApplicationDbContext> options,
         IOptions<OperationalStoreOptions> operationalStoreOptions,
         IMediator mediator,
         AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor)
-        : base(options)
+        : base(options, operationalStoreOptions)
     {
         _mediator = mediator;
         _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
-        _operationalStoreOptions = operationalStoreOptions;
     }
 
     public DbSet<TodoList> TodoLists => Set<TodoList>();
 
     public DbSet<TodoItem> TodoItems => Set<TodoItem>();
 
-    /// <summary>
-    /// Gets or sets the user sessions.
-    /// </summary>
-    /// <value>
-    /// The keys.
-    /// </value>
-    public DbSet<ServerSideSession> ServerSideSessions { get; set; }
-
-    /// <summary>
-    /// Gets or sets the <see cref="DbSet{PersistedGrant}"/>.
-    /// </summary>
-    public DbSet<PersistedGrant> PersistedGrants { get; set; }
-
-    /// <summary>
-    /// Gets or sets the <see cref="DbSet{DeviceFlowCodes}"/>.
-    /// </summary>
-    public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
-
-    /// <summary>
-    /// Gets or sets the <see cref="DbSet{Key}"/>.
-    /// </summary>
-    public DbSet<Key> Keys { get; set; }
-
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         base.OnModelCreating(builder);
-        builder.ConfigurePersistedGrantContext(_operationalStoreOptions.Value);
 
         // Addressing issue with SnakeCaseNamingConvention not applying to Identity
         // tables as specified here:
